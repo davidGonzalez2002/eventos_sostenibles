ca565e2680badab9fd53bdb4048508b94ca4819e
davidGonzalez2002 <davidgonzalezsierra13@gmail.com>
2025-05-29 13:53:44 +0200

    add pom.xml

--- src/main/java/com/indra/eventossostenibles/Main.java
+++ src/main/java/com/indra/eventossostenibles/Main.java
@@ -2,6 +2,6 @@ package com.indra.eventossostenibles;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola, mundo!");
     }
 }
