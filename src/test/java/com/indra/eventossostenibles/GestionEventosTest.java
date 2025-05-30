5289c4a92fdb4a68dd7a667c54748cac01673fd9
davidGonzalez2002 <davidgonzalezsierra13@gmail.com>
2025-05-30 12:22:14 +0200

    Update readme and optimize imports

--- src/test/java/com/indra/eventossostenibles/GestionEventosTest.java
+++ src/test/java/com/indra/eventossostenibles/GestionEventosTest.java
@@ -1,11 +1,9 @@
 package com.indra.eventossostenibles;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class GestionEventosTest {
     private GestionEventos gestionEventos;
--- src/test/java/com/indra/eventossostenibles/OrganizadorTest.java
+++ src/test/java/com/indra/eventossostenibles/OrganizadorTest.java
@@ -3,7 +3,6 @@ package com.indra.eventossostenibles;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrganizadorTest {
 
